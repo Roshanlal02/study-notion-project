df7a3e81d8069ce0b725de46f462fd66e47d086a
Roshan Lal D <roshanlald02@gmail.com>
2023-08-25 22:51:29 +0530

    About us page contact component

--- src/components/core/About/ContactFormSection.jsx
+++ src/components/core/About/ContactFormSection.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ContactUsForm from "../../common/ContactUsForm"
+import React from "react";
+import ContactUsForm from "../../common/ContactUsForm";
 
 const ContactFormSection = () => {
   return (
     <div>
-    <ContactUsForm />
+      <h1 className="mb-3 text-center text-4xl font-semibold text-richblack-5">
+        Get in Touch
+      </h1>
+      <p className="mb-8 text-richblack-300 font-medium text-base text-center">
+        We’d love to here for you, Please fill out this form.
+      </p>
+      <ContactUsForm />
     </div>
-  )
-}
+  );
+};
 
-export default ContactFormSection
+export default ContactFormSection;
