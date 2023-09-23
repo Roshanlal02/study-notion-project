742bcc35f77e1640945b2c1f674f5a876f58e039
Roshan Lal D <roshanlald02@gmail.com>
2023-09-23 19:33:51 +0530

    Add course > Course Information page

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -3,9 +3,8 @@ import RenderSteps from "./RenderSteps";
 export default function AddCourse() {
   return (
     <>
-      <div className="text-white flex gap-8">
+      <div className="text-white flex gap-8 mt-8">
         <div className="w-3/5">
-          <h1>Add Course</h1>
           <div>
             <RenderSteps />
           </div>
